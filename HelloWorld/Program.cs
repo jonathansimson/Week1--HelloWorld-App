523af415517f7f1503a432c29e98129a14b4ffeb
Jonathan Simson <jonathan@tktk.ee>
2022-02-15 20:23:31 +0200

    Hello World Program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is you name?");
+            string UserName;
+
+            UserName = Console.ReadLine();
+            Console.WriteLine("Welcome,"+ UserName);
+            Console.Read();
+            
+            
+        }
+    }
+}
